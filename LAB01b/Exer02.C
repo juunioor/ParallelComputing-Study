5f0a332431206b4a0f886806a6f77b1238eea6fa
Matheus Henrique <73514316+matheusapostulo@users.noreply.github.com>
2022-08-19 09:45:39 -0300

    Update Exer02.C

--- LAB01b/Exer02.C
+++ LAB01b/Exer02.C
@@ -1,25 +1,30 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    int arrayNotas[3] = {5,5,5};
-    
-    // Calculando a média das notas
-    float notaTotal = 0;
-    int i;
-    for(i = 0; i < 3; i++){
-        notaTotal += arrayNotas[i];
-    }
-    float media = notaTotal / 3;
-    printf("media %i", media);
+  int arrayNotas[3] = {8,10,5};
+  
+  // Calculando a média das notas
+  float notaTotal = 0;
+  for(int i = 0; i < 3; i++){
+    notaTotal += arrayNotas[i];
+  }
+  float media = notaTotal / 3;
 
-    // Calculando a Média de Aproveitamento
-    float mediaAproveitamento = array[0] + (array[1]*2) + (array[2]*3) + media;
-    
-    // Classificando a media de Aproveitamento
-    if(mediaAproveitamento >= 9){
-        printf()
-    } else if(mediaAproveitamento){
-    }
-    return 0;
+  // Calculando a Média de Aproveitamento
+  float mediaAproveitamento = (arrayNotas[0] + (arrayNotas[1]*2) + (arrayNotas[2]*3) + media) /7;
+  
+  // Classificando a media de Aproveitamento
+  if(mediaAproveitamento >= 9){
+    cout << "Com a Media de aproveitamento " << mediaAproveitamento << " o conceito da nota e A";
+  } else if(mediaAproveitamento >= 7.5 and mediaAproveitamento < 9){
+    cout << "Com a Media de aproveitamento " << mediaAproveitamento << " o conceito da nota e B";
+  } else if(mediaAproveitamento >= 6 and mediaAproveitamento < 7.5){
+    cout << "Com a Media de aproveitamento " << mediaAproveitamento << " o conceito da nota e C";
+  } else if(mediaAproveitamento >= 4 and mediaAproveitamento < 6){
+    cout << "Com a Media de aproveitamento " << mediaAproveitamento << " o conceito da nota e D";
+  } else if(mediaAproveitamento < 4){
+    cout << "Com a Media de aproveitamento " << mediaAproveitamento << " o conceito da nota e E";
+  }
 }
