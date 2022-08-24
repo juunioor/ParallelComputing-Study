111351fbc02e1a231dc1a37e483155b0c7d35c51
Valdir Junior <62557235+juunioor@users.noreply.github.com>
2022-08-24 20:07:33 -0300

    Add files via upload

---
+++ LAB01b/Exer12.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int retornaPosicao(string string_usuario, char caracter_usuario){
+  int tamanho_string = sizeof(string_usuario)/sizeof(string_usuario[0]); 
+  for(int i = 0; i < tamanho_string; i++){
+    if (string_usuario[i] == caracter_usuario){
+      return i+1;  
+    }
+  }
+  return -1;
+}
+
+int main(){
+  string string_usuario;
+  char caracter_usuario;
+  cout << "Digite a string: " << endl;
+  cin >> string_usuario;
+  cout << "Digite o char para pesquisar na string digitada: " << endl;
+  cin >> caracter_usuario;
+
+  int posicao_caracter;
+  posicao_caracter = retornaPosicao(string_usuario, caracter_usuario);
+
+  if(posicao_caracter != -1){
+    cout << "A posição do caracter digitado na string é " << posicao_caracter << "!";
+  } else {
+    cout << "O caracter digitado não está na string digitada!";
+  }    
+}
