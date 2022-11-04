f98107085321eb45faeb138dff61eb408aadc535
Matheus Henrique <73514316+matheusapostulo@users.noreply.github.com>
2022-11-04 18:42:28 -0300

    Update mult_matr_1.c

--- LAB07b/mult_matr_1.c
+++ LAB07b/mult_matr_1.c
@@ -3,12 +3,34 @@
 #include <omp.h>
 
 int main(int argc, char* argv[]) {
-    int linhas = 6;
-    int colunas = 6;
+    int linhas = 1000;
+    int colunas = 1000;
     
-    double A[6][6] = {{1.21,7.32,5.43,8.54,2.64,2.77},{1.21,9.64,2.54,3.77,2.32,2.77},{1.21,6.32,5.64,4.77,2.54,2.77},{2.21,3.64,9.77,2.54,2.32,2.77},{1.21,6.64,5.54,4.32,2.77,2.77},{2.77,3.32,9.77,2.21,2.64,2.77}}; // matriz  
+    double A[1000][1000];
+    double x[1000][1000];
+    double y[1000];
+    
+    double numero_p_A = 1.123;
+    double numero_p_x = 1.321;
+    
+    // POPULANDO MATRIZ A
+    for(int i = 0; i < linhas; i++){
+        for(int j = 0; j < colunas; j++){
+            A[i][j] = (i + j) * numero_p_A;
+        }    
+    }
+    
+    // POPULANDO MATRIZ x
+    for(int i = 0; i < linhas; i++){
+        for(int j = 0; j < colunas; j++){
+            x[i][j] = (i + j) * numero_p_x;
+        }    
+    }
+    
+    
+    /*double A[6][6] = {{1.21,7.32,5.43,8.54,2.64,2.77},{1.21,9.64,2.54,3.77,2.32,2.77},{1.21,6.32,5.64,4.77,2.54,2.77},{2.21,3.64,9.77,2.54,2.32,2.77},{1.21,6.64,5.54,4.32,2.77,2.77},{2.77,3.32,9.77,2.21,2.64,2.77}}; // matriz  
     double x[6] = {5.43,6.21,3.54,1.64,2.32,2.77};         // vetor 
-    double y[6] = {0.0,0.0,0.0,0.0,0.0,0.0}; // resultado
+    double y[6] = {0.0,0.0,0.0,0.0,0.0,0.0}; // resultado */
     
     int qtd_thread = strtol(argv[1], NULL, 10); // variável de quantidade de threads
     
