be57ea478289595c6baa430742a2d434e57338b7
Matheus Henrique <73514316+matheusapostulo@users.noreply.github.com>
2022-11-04 18:18:01 -0300

    Update trapezio1.c

--- LAB07b/trapezio1.c
+++ LAB07b/trapezio1.c
@@ -20,7 +20,6 @@ int main(int argc, char* argv[])
     int thread_count;
     
     thread_count = strtol(argv[1], NULL, 10);
-    printf("\n%ld argv de n\n", argv[2]);
     n = strtol(argv[2], NULL, 10);
     //printf("Entre com o valor a, b e n\n");
     //scanf("%lf %lf %d", &a, &b, &n);
