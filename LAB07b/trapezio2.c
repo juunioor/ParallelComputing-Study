b20c16ce19e38e3c37707c61947523a83978adab
Matheus Henrique <73514316+matheusapostulo@users.noreply.github.com>
2022-11-04 18:12:38 -0300

    Update trapezio2.c

--- LAB07b/trapezio2.c
+++ LAB07b/trapezio2.c
@@ -16,10 +16,11 @@ int main(int argc, char* argv[])
     //double a,b;
     double a = 0;
     double b = 1;
-    int n = strtol(argv[2], NULL, 10);    
+    int n;    
     int thread_count;
     
     thread_count = strtol(argv[1], NULL, 10);
+    n = strtol(argv[2], NULL, 10);
     //printf("Entre com o valor a, b e n\n");
     //scanf("%lf %lf %d", &a, &b, &n);
     
