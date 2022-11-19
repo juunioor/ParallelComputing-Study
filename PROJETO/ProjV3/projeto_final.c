a16b0f3ccc5cfa881c378cce79d5b00b5e5912f6
Matheus Henrique <73514316+matheusapostulo@users.noreply.github.com>
2022-11-19 13:52:29 -0300

    Update projeto_final.c

--- PROJETO/ProjV3/projeto_final.c
+++ PROJETO/ProjV3/projeto_final.c
@@ -34,8 +34,8 @@ int main(int  argc, char *argv[])
       // As threads vão intercalar o cálculo baseado em seu id
       for(i = id_thread; i < iteracoes; i = i+qtd_thread){
           res += 1.0/fatorial(i);
-          if (i == 5000){
-              printf("\nFATORIAL DE 5000 = %Lf", fatorial(i));
+          if (i == 2000){
+              printf("\nFATORIAL DE 2000 = %Lf", fatorial(i));
           }    
       }
     }
