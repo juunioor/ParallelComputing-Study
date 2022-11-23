60cbcfb317ff3fcced7e890bbdbb085adddd1b0a
Matheus Henrique <73514316+matheusapostulo@users.noreply.github.com>
2022-11-23 20:10:24 -0300

    Update projeto_final.c

--- PROJETO/ProjV4 (Final)/projeto_final.c
+++ PROJETO/ProjV4 (Final)/projeto_final.c
@@ -23,7 +23,7 @@ int main(int  argc, char *argv[])
     
     //inicializando res com GMP
     mpf_t res;
-    mpf_init2(res,3000000);
+    mpf_init2(res,2500000);
     mpf_set_str(res,"0.0",10);
     
     int iteracoes = atoi(argv[1]);
@@ -34,7 +34,7 @@ int main(int  argc, char *argv[])
     { 
       // variável intermediária para adicionar ao resultado principal depois na zona crítica
       mpf_t res_temp;
-      mpf_init2(res_temp, 3000000);
+      mpf_init2(res_temp, 2500000);
       mpf_set_str(res_temp, "0.0", 10);
         
       int id_thread = omp_get_thread_num();
@@ -47,16 +47,16 @@ int main(int  argc, char *argv[])
     
       // inicilizando onde o fatorial irá retornar
       mpf_t res_fatorial;
-      mpf_init2(res_fatorial,3000000);
+      mpf_init2(res_fatorial,2500000);
       
       // inicializando a variavel que vai receber o resultado de 1/fatorial(i)
       mpf_t um_dividido_i;
-      mpf_init2(um_dividido_i,3000000);
+      mpf_init2(um_dividido_i,2500000);
       mpf_set_str(um_dividido_i, "0.0", 10);  
         
       // inicializando uma variável pro valor 1
       mpf_t valor_um;
-      mpf_init2(valor_um,3000000);
+      mpf_init2(valor_um,2500000);
       mpf_set_str(valor_um, "1.0", 10);
         
       // Chamando o fatorial pra calcular o primeiro fatorial e o primeiro res da thread que está chamando.
